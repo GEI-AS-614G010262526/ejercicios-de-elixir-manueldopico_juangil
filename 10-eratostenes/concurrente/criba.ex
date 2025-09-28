20b8085ecf62f2988e0ae9deb658bd9fcc7a44fc
JuanGilArregui <juan.gil.arregui@udc.es>
2025-09-28 19:40:25 +0200

    concurrente-1 listo

--- 10-eratostenes/concurrente/criba.ex
+++ 10-eratostenes/concurrente/criba.ex
@@ -1,23 +1,60 @@
 defmodule Criba do
   def primo(n) do
+    criba(n)
   end
 
-  def filtro(prime, next) do
+  def rango(x, m) when x > m, do: []
 
+  def rango(x, m), do: [x | rango(x + 1, m)]
+
+  def criba(n) do
+    task = Task.async(fn -> colector() end)
+    send(task.pid, {:prime, 2})
+
+    first = spawn(fn -> filtro(2, nil, task.pid) end)
+
+    Enum.each(rango(3, n), fn x ->
+      send(first, {:number, x})
+    end)
+
+    send(first, {:done, task.pid})
+
+    Task.await(task)
+  end
+
+  def filtro(prime, next, colector) do
     receive do
       {:number, num} ->
-        if rem(num, prime) == 0 do
-          filtro(prime, next)
-        else
-          if next == nil do
-          new_next = spawn(fn -> filtro(num, nil) end)
-          filtro(prime, new_next)
-          else
+        cond do
+          rem(num, prime) == 0 ->
+            filtro(prime, next, colector)
+
+          next == nil ->
+            send(colector, {:prime, num})
+            new_next = spawn(fn -> filtro(num, nil, colector) end)
+            filtro(prime, new_next, colector)
+
+          true ->
             send(next, {:number, num})
-            filtro(prime, next)
-          end
+            filtro(prime, next, colector)
+        end
+
+      {:done, colector} ->
+        if next != nil do
+          send(next, {:done, colector})
+        else
+          send(colector, :done)
         end
     end
   end
-  
+
+  def colector(primes \\ []) do
+    receive do
+      {:prime, p} ->
+        colector([p | primes])
+
+      :done ->
+        Enum.reverse(primes)
+    end
+  end
 end
