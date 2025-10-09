3c455b7515cf544556ca3d7274ce60ba6c556b41
JuanGilArregui <juan.gil.arregui@udc.es>
2025-10-09 19:32:56 +0200

    ejercicio 2, falta probarlo

--- 20-pool_de_trabajadores/pool_workers.ex
+++ 20-pool_de_trabajadores/pool_workers.ex
@@ -7,8 +7,7 @@ defmodule Servidor do
   end
 
   @spec run_batch(pid(), list()) :: list()
-  # el resultado se devuelve cuando todos los trabajos han devuelto su resultado,
-  # tiene que estar ordenado
+  # gestiona un lote de trabajos, devolviendo error si aun no ha terminado
   def run_batch(master, jobs) do
     # validar el trabajo
     send(master, {:trabajos, self(), jobs})
@@ -30,10 +29,11 @@ defmodule Servidor do
 
   # ==== funciones privadas ====
 
+  # funcion que crea la cantidad de trabajadores deseada
   defp init(num_workers) do
     workers =
       1..num_workers
-      |> Enum.map(fn ->
+      |> Enum.map(fn _ ->
         spawn(fn -> Worker.loop() end)
       end)
 
@@ -42,68 +42,132 @@ defmodule Servidor do
 
   defp loop(workers, ocupado) do
     receive do
-      # recibe una lista de listas, para cada lista crea un batch
+      # para cada trabajo crea un batch
       {:trabajos, from, trabajos} ->
-        cod do
+        cond do
+          # si ya hay un trabajo en proceso no acepta el trabajo
           ocupado ->
             send(from, {:error, :ocupado})
             loop(workers, ocupado)
 
+          # si esta libre crea un proceso para el trabajo
           true ->
-            spawn(batch(from, trabajos))
-            loop(workers, ocupado)
+            parent = self()
+            spawn(fn -> batch(parent, from, workers, trabajos) end)
+            loop(workers, true)
         end
+
+      :batch_done ->
+        loop(workers, false)
+
+      {:stop, from} ->
+        Enum.each(workers, fn w -> send(w, :stop) end)
+        send(from, :ok)
     end
   end
 
-  # se acuerda del orden y ejecuta los trabajos
-  defp batch(from, trabajos) do
+  # se acuerda del orden con un index y ejecuta los trabajos
+  # ordena el resultado y lo envia
+  defp batch(parent, from, workers, trabajos) do
+    # se crean tuplas con el trabajo y un index
     trabajos_with_index = Enum.with_index(trabajos)
-    resultados = ejecutar_trabajos(trabajos_with_index, [])
+    resultados = ejecutar_trabajos(trabajos_with_index, workers, [])
 
-    # TODO recibir resultados y ordenarlos con su index
+    resultados_ordenados =
+      resultados
+      # ordena con el index
+      |> Enum.sort_by(fn {idx, _} -> idx end)
+      # elimina el index y devuelve solo el resultado
+      |> Enum.map(fn {_, resto} -> resto end)
 
-    send(from, {:resultados, resultados})
+    send(from, {:resultados, resultados_ordenados})
+    send(parent, :batch_done)
   end
 
-  def ejecutar_trabajos(tuplas_trabajos, resultados) do
-    #TODO terminar 
-    cond do
-      length(tuplas_trabajos) == length(resultados) ->
-        resultados
+  def ejecutar_trabajos(jobs_with_index, _workers, _accumulated_results) do
+    Enum.each(_workers, fn w -> send(w, {:work_todo, self()}) end)
+    assign_and_wait(jobs_with_index, [], [])
+  end
 
-      true ->
-        send(:work_todo)
+  # no quedan trabajos + no faltan resultados -> se devuelve
+  defp assign_and_wait([], [], resultado_final) do
+    resultado_final
+  end
 
-        receive do
-          {:free, worker_pid} ->
-            send(worker_pid, {:trabajo, List.first(tuplas_trabajos)})
-            ejecutar_trabajos(List.delete(list, 0), resultados)
+  defp assign_and_wait(trabajo_pend, resultado_pend, resultado_final) do
+    receive do
+      {:available, w} ->
+        # si queda trabajo se envia a un trabajador libre
+        case trabajo_pend do
+          [{job, index} | job_tail] ->
+            send(w, {:trabajo, self(), job})
+            # llamamos de nuevo a la funcion sin la cabeza de los trabajos
+            # asignamos el index del trabajo a resultados pendientes
+            assign_and_wait(
+              job_tail,
+              [{index, w} | resultado_pend],
+              resultado_final
+            )
 
-          {:occupied} ->
-            ejecutar_trabajos(tupla_trabajos, resultados)
+          # si no quedan trabajos pendientes descartamos el resto de mensajes
+          # y llamamos de nuevo a la funcion a la espera de los resultados
+          [] ->
+            flush_available()
+            assign_and_wait(trabajo_pend, resultado_pend, resultado_final)
         end
-    end
 
-    # reservar worker
-    # si recibe ok mandar trabajo
+      # si se recibe un resultado se elimina el valor en resultado pendiente
+      # y se añade al resultado final
+      {:result, worker_pid, result} ->
+        # Separamos el trabajo completado del resto
+        {[{{idx, _}, _}], restantes} =
+          resultado_pend
+          |> Enum.with_index()
+          |> Enum.split_with(fn {{_, pid}, _} -> pid == worker_pid end)
+
+        new_resultados_pend = Enum.map(restantes, fn {{index, pend}, _} -> {index, pend} end)
+
+        assign_and_wait(
+          trabajo_pend,
+          new_resultados_pend,
+          [{idx, result} | resultado_final]
+        )
+    after
+      # si no se recibe ninguna respuesta se vuelve a llamar la funcion
+      500 ->
+        assign_and_wait(trabajo_pend, resultado_pend, resultado_final)
+    end
+  end
+
+  # descartar mensajes
+  defp flush_available() do
+    receive do
+      {:available, _} -> flush_available()
+    after
+      0 -> :ok
+    end
   end
 end
 
-# defmodule Worker do
-#   def loop() do
-#     receive do
-#       {:trabajo, from, func} ->
-#         result = proccess(func)
-#         send(from, {:result, self(), result})
-#         loop()
+defmodule Worker do
+  def loop() do
+    receive do
+      {:trabajo, from, func} ->
+        value = func.()
+        result = proccess(value)
+        send(from, {:result, self(), result})
+        loop()
 
-#       :stop ->
-#         :ok
-#     end
-#   end
+      {:work_todo, from} ->
+        send(from, {:available, self()})
+        loop()
 
-#   def proccess(func) do
-#     # trabajar
-#   end
-# end
+      :stop ->
+        :ok
+    end
+  end
+
+  def proccess(x) do
+    x * 2
+  end
+end
