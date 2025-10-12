c09e6d5d295df388418f046db1cd9e0399bc1b79
Manuel Dopico Caneiro <manuel.dopico.caneiro@udc.es>
2025-10-12 12:46:54 +0200

    version distribuida gestor

--- 30-gestor_recursos/gestor_recursos_1.ex
+++ 30-gestor_recursos/gestor_recursos_1.ex
@@ -1,3 +1,4 @@
+##1. No distribuido
 defmodule Servidor do
 
   ##start del servidor
@@ -24,7 +25,7 @@ defmodule Servidor do
       end
 
       {:release,from,recurso}-> case Map.get(asignados, recurso) do
-        ^from ->
+        from ->
           nuevo_asignados = Map.delete(asignados, recurso)
           send(from, :ok)
           loop([recurso|disponibles],nuevo_asignados)
